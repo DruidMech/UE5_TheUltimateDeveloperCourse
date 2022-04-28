f70b25cf89ee2fd464cc321d5863c5a20b87486d
Stephen Ulibarri <druidmech@gmail.com>
2022-04-28 17:05:21 -0600

    Drawing Debug Lines

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -16,34 +16,17 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
-	}
-
 	UWorld* World = GetWorld();
-
 	FVector Location = GetActorLocation();
-	DRAW_SPHERE(Location)
+	FVector Forward = GetActorForwardVector();
 
+	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 	
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime);
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
 }
 
--- Source/Slash/Slash.h
+++ Source/Slash/Slash.h
@@ -5,3 +5,4 @@
 #include "CoreMinimal.h"
 
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
