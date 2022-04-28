a39d700ac93149ee9399feefa2a4c871536dc7ce
Stephen Ulibarri <druidmech@gmail.com>
2022-04-28 15:08:44 -0600

    Onscreen Debug Messages

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -13,6 +13,11 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
+	}
 }
 
 void AItem::Tick(float DeltaTime)
