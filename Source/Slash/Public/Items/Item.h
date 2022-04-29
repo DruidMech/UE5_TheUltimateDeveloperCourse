1cd45b2e3ca1a40c2ec7ba5f804f1f8c5823f59a
Stephen Ulibarri <druidmech@gmail.com>
2022-04-29 01:02:03 -0600

    Exposing Variables to Blueprint

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -18,6 +18,10 @@ protected:
 	virtual void BeginPlay() override;
 private:
 	float RunningTime;
+
+	UPROPERTY(EditAnywhere)
 	float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere)
 	float TimeConstant = 5.f;
 };
