620adf7a0e346843c6f9e2f29c3be2dd57ff0314
Stephen Ulibarri <druidmech@gmail.com>
2022-04-28 19:21:55 -0600

    SetActorRotation

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -16,6 +16,7 @@ void AItem::BeginPlay()
 	UWorld* World = GetWorld();
 
 	SetActorLocation(FVector(0.f, 0.f, 50.f));
+	SetActorRotation(FRotator(0.f, 45.f, 0.f));
 	FVector Location = GetActorLocation();
 	FVector Forward = GetActorForwardVector();
 	DRAW_SPHERE(Location);
