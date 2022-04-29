5c3936b05dcad40c6f37127916bb7fcd46199eb9
Stephen Ulibarri <druidmech@gmail.com>
2022-04-28 19:14:59 -0600

    SetActorLocation

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -14,9 +14,10 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	UWorld* World = GetWorld();
+
+	SetActorLocation(FVector(0.f, 0.f, 50.f));
 	FVector Location = GetActorLocation();
 	FVector Forward = GetActorForwardVector();
-
 	DRAW_SPHERE(Location);
 	//DRAW_LINE(Location, Location + Forward * 100.f);
 	//DRAW_POINT(Location + Forward * 100.f);
