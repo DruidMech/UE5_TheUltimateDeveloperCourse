a4906f1257b8d9d1c8c30194eacf0dc9546ffbf5
Stephen Ulibarri <druidmech@gmail.com>
2022-05-25 14:48:42 -0600

    Unreal Interfaces

---
+++ Source/Slash/Private/Interfaces/HitInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/HitInterface.h"
+
+// Add default functionality here for any IHitInterface functions that are not pure virtual.
