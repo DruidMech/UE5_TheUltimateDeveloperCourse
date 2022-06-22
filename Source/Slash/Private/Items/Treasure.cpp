81bd163c796e521d68b563afbb2c3c0f04f9d334
Stephen Ulibarri <druidmech@gmail.com>
2022-06-21 21:12:27 -0600

    Treasure

---
+++ Source/Slash/Private/Items/Treasure.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Treasure.h"
+#include "Characters/SlashCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATreasure::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ASlashCharacter* SlashCharacter = Cast<ASlashCharacter>(OtherActor);
+	if (SlashCharacter)
+	{
+		if (PickupSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				PickupSound,
+				GetActorLocation()
+			);
+		}
+		Destroy();
+	}
+}
---
+++ Source/Slash/Public/Items/Treasure.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "Treasure.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API ATreasure : public AItem
+{
+	GENERATED_BODY()
+protected:
+
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+private:
+
+	UPROPERTY(EditAnywhere, Category = Sounds)
+	USoundBase* PickupSound;
+};
