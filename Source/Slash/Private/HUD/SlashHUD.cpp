39c4d669abddbaac99dee1e9b97d1d6ddae19a48
Stephen Ulibarri <druidmech@gmail.com>
2022-06-30 10:27:13 -0600

    Slash HUD

---
+++ Source/Slash/Private/HUD/SlashHUD.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/SlashHUD.h"
+#include "HUD/SlashOverlay.h"
+
+void ASlashHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		APlayerController* Controller = World->GetFirstPlayerController();
+		if (Controller && SlashOverlayClass)
+		{
+			SlashOverlay = CreateWidget<USlashOverlay>(Controller, SlashOverlayClass);
+			SlashOverlay->AddToViewport();
+		}
+	}
+}
---
+++ Source/Slash/Public/HUD/SlashHUD.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SlashHUD.generated.h"
+
+class USlashOverlay;
+
+UCLASS()
+class SLASH_API ASlashHUD : public AHUD
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditDefaultsOnly, Category = Slash)
+	TSubclassOf<USlashOverlay> SlashOverlayClass;
+
+	UPROPERTY()
+	USlashOverlay* SlashOverlay;
+	
+};
